4d0ec7ae554babb7a8f9a770cd5914b0328d1d2f
tyler <tyler.mapp@harness.io>
2026-04-10 13:30:27 -0400

    test: update to try things in ccm direct

--- aws/ecs/main.tf
+++ aws/ecs/main.tf
@@ -1,10 +1,10 @@
 locals {
   name = var.name == null ? random_pet.name.id : lower(var.name)
   # use route53 domain if given, otherwise fallback to default ALB DNS name
-  lb_hostname = var.hostedzone == null ? aws_lb.alb[0].dns_name : "${local.name}.${data.aws_route53_zone.zone[0].name}"
-  tags = {
-    lb_hostname = local.lb_hostname
-  }
+  # lb_hostname = var.hostedzone == null ? aws_lb.alb[0].dns_name : "${local.name}.${data.aws_route53_zone.zone[0].name}"
+  # tags = {
+  #   lb_hostname = local.lb_hostname
+  # }
 }
 
 data "harness_platform_current_account" "current" {}
@@ -14,14 +14,14 @@ data "aws_route53_zone" "zone" {
   zone_id = var.hostedzone
 }
 
-resource "aws_route53_record" "alb" {
-  count   = var.hostedzone == null ? 0 : 1
-  zone_id = data.aws_route53_zone.zone[0].zone_id
-  name    = local.lb_hostname
-  type    = "CNAME"
-  ttl     = "60"
-  records = [aws_lb.alb[0].dns_name]
-}
+# resource "aws_route53_record" "alb" {
+#   count   = var.hostedzone == null ? 0 : 1
+#   zone_id = data.aws_route53_zone.zone[0].zone_id
+#   name    = local.lb_hostname
+#   type    = "CNAME"
+#   ttl     = "60"
+#   records = [aws_lb.alb[0].dns_name]
+# }
 
 resource "random_pet" "name" {
   keepers = {
--- aws/ecs/output.tf
+++ aws/ecs/output.tf
@@ -3,22 +3,22 @@ output "name" {
   description = "Name of the ec2 instance"
 }
 
-output "alb-url" {
-  value       = "http://${aws_lb.alb[0].dns_name}"
-  description = "URL of the ALB"
-}
+# output "alb-url" {
+#   value       = "http://${aws_lb.alb[0].dns_name}"
+#   description = "URL of the ALB"
+# }
 
 output "service" {
   value       = aws_ecs_service.service.id
   description = "ARN of the ECS service"
 }
 
-output "rule" {
-  value       = "https://app.harness.io/ng/account/${data.harness_platform_current_account.current.id}/ce/autostopping-rules/rule/${harness_autostopping_rule_ecs.rule.id}"
-  description = "Link to autostopping rule in Harness"
-}
+# output "rule" {
+#   value       = "https://app.harness.io/ng/account/${data.harness_platform_current_account.current.id}/ce/autostopping-rules/rule/${harness_autostopping_rule_ecs.rule.id}"
+#   description = "Link to autostopping rule in Harness"
+# }
 
-output "url" {
-  value       = harness_autostopping_rule_ecs.rule.custom_domains[0]
-  description = "URL for the application"
-}
+# output "url" {
+#   value       = harness_autostopping_rule_ecs.rule.custom_domains[0]
+#   description = "URL for the application"
+# }
